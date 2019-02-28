6429000f4f85ebb62f2450d7d1ba0a96b8cb4010
Hariet <harietlewis@gmail.com>
2019-02-28 11:19:59 -0500

    Colors are Workingg

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function makeRow() {
 
 let addRow = document.getElementById('add-row');
 addRow.addEventListener('click', makeRow);
+
+table.addEventListener('click', colorize);
+
+function colorize(event) {
+//console.log('Clicked!')
+let cell = event.target;
+  if (cell.className.length) {
+  cell.className = '';
+} else {
+cell.className = currentColor;
+ }
+}
+
+let colors = document.getElementsByTagName('select')[0];
+colors.addEventListener('change', changeColor);
+
+let currentColor = 'red';
+function changeColor(event) {
+  let cell = event.target.value;
+  currentColor = cell;
+  //console.log(cell)
+
+}
