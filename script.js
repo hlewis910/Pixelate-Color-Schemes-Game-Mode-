902e2492e88d7491e53077d9dd78aa9e487cf312
Katie Agresta <katieagresta1@gmail.com>
2019-02-28 10:31:27 -0500

    first attempts

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your code here
+let table = document.getElementsByTagName('table')[0];
+
+function makeRow() {
+  let newRow = document.createElement('tr');
+  table.appendChild(newRow);
+  for (let i = 0; i < 21; i++) {
+    let newCell = document.createElement('td');
+    newRow.appendChild(newCell);
+  }
+}
+
+let addRow = document.getElementById('add-row');
+addRow.addEventListener('click', makeRow);
