3d42717693a72bde11d4467a06c0a3d8aee8c0ef
Hariet <harietlewis@gmail.com>
2019-07-30 14:57:03 -0400

    MouseOver functions

--- script.js
+++ script.js
@@ -1,85 +1,124 @@
-// Your code here
-let table = document.getElementsByTagName('table')[0];
-let currentColor = 'purple';
+const table = document.getElementsByTagName('table')[0]
+const select = document.getElementsByTagName('select')[0]
+const button = document.getElementById('add-row')
+let chosenColor = 'purple'
 
-let submit = document.getElementById('button');
-button.addEventListener('click', setDimensions );
+button.addEventListener('click', makeRow)
+table.addEventListener('click', colorize)
+table.addEventListener('mousedown', addMouseOver)
+table.addEventListener('mouseup', removeMouseOver)
+select.addEventListener('change', pickColor)
 
-function setDimensions() {
-  function makeRow() {
-    let newRow = document.createElement('tr');
-    table.appendChild(newRow);
-    for (let i = 0; i < 20; i++) {
-      let newCell = document.createElement('td');
-      newRow.appendChild(newCell);
-    }
+function makeRow() {
+  const row = document.createElement('tr')
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement('td')
+    row.appendChild(td)
   }
-}
-
-
-makeRow();
-makeRow();
-makeRow();
-makeRow();
-makeRow();
-
-
-let addRow = document.getElementById('add-row');
-addRow.addEventListener('click', makeRow);
-
-table.addEventListener('click', colorize);
-table.addEventListener('mouseover', colorize);
-
-let entireGrid = document.getElementById('one-color');
-entireGrid.addEventListener('click', gridColor);
-
-function gridColor() {
-  let allCells = Array.from(document.getElementsByTagName('TD'));
-  allCells.forEach(function(elem) {
-    elem.className = currentColor;
-  });
-}
-
-let clearButton = document.getElementById('clear');
-clear.addEventListener('click', clearColor);
-
-function clearColor() {
-  let allCells = Array.from(document.getElementsByTagName('TD'));
-  allCells.forEach(function(elem) {
-    elem.className = '';
-  });
-}
-
-let autofill = document.getElementById('fill');
-autofill.addEventListener('click', autoColor);
-
-function autoColor() {
-  console.log('this is working')
-  let allCells = Array.from(document.getElementsByTagName('TD'));
-  allCells.forEach(function(elem) {
-  if (elem.className === '') {
-     elem.className = currentColor;
-   }
- })
+  table.append(row)
 }
 
 function colorize(event) {
-  //console.log('Clicked!')
-  let cell = event.target;
-  if (cell.tagName === 'TD') {
-    if (cell.className === currentColor) {
-      cell.className = '';
-    } else {
-      cell.className = currentColor;
-    }
+  const target = event.target
+  if (target.tagName !== 'TD') {
+    return
+  }
+  if (target.className === 'chosenColor') {
+    target.className = ''
+  } else {
+    target.className = chosenColor
   }
 }
 
-let colors = document.getElementsByTagName('select')[0];
-colors.addEventListener('change', changeColor);
 
-function changeColor(event) {
-  let cell = event.target.value;
-  currentColor = cell;
-  //console.log(cell)
+function pickColor(event) {
+  chosenColor = event.target.value
 }
+
+function addMouseOver() {
+  table.addEventListener('mouseover', colorize)
+}
+
+function removeMouseOver() {
+  table.removeEventListener('mouseover', colorize)
+}
+
+// const table = document.getElementsByTagName('table')[0];
+// let currentColor = 'purple';
+
+// let submit = document.getElementById('button');
+// button.addEventListener('click', setDimensions );
+
+// function setDimensions() {
+//   function makeRow() {
+//     let newRow = document.createElement('tr');
+//     table.appendChild(newRow);
+//     for (let i = 0; i < 20; i++) {
+//       let newCell = document.createElement('td');
+//       newRow.appendChild(newCell);
+//     }
+//   }
+// }
+
+
+// makeRow();
+// makeRow();
+
+
+// let addRow = document.getElementById('add-row');
+// addRow.addEventListener('click', makeRow);
+
+// table.addEventListener('click', colorize);
+// table.addEventListener('mouseover', colorize);
+
+// let entireGrid = document.getElementById('one-color');
+// entireGrid.addEventListener('click', gridColor);
+
+// function gridColor() {
+//   let allCells = Array.from(document.getElementsByTagName('TD'));
+//   allCells.forEach(function(elem) {
+//     elem.className = currentColor;
+//   });
+// }
+
+// let clearButton = document.getElementById('clear');
+// clear.addEventListener('click', clearColor);
+
+// function clearColor() {
+//   let allCells = Array.from(document.getElementsByTagName('TD'));
+//   allCells.forEach(function(elem) {
+//     elem.className = '';
+//   });
+// }
+
+// let autofill = document.getElementById('fill');
+// autofill.addEventListener('click', autoColor);
+
+// function autoColor() {
+//   console.log('this is working')
+//   let allCells = Array.from(document.getElementsByTagName('TD'));
+//   allCells.forEach(function(elem) {
+//   if (elem.className === '') {
+//      elem.className = currentColor;
+//    }
+//  })
+// }
+
+// function colorize(event) {
+//   let cell = event.target;
+//   if (cell.tagName === 'TD') {
+//     if (cell.className === currentColor) {
+//       cell.className = '';
+//     } else {
+//       cell.className = currentColor;
+//     }
+//   }
+// }
+
+// let colors = document.getElementsByTagName('select')[0];
+// colors.addEventListener('change', changeColor);
+
+// function changeColor(event) {
+//   let cell = event.target.value;
+//   currentColor = cell;
+// }
